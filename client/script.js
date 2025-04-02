0eda3014c24ab169a249f670cd6a5689ce5bb31f
Divyansh Joshi <divyanshjoshi@Divyanshs-MacBook-Air.local>
2025-04-02 14:00:22 +0530

    Added a counter for visitors

--- client/script.js
+++ client/script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const resultParagraph = document.getElementById("result-paragraph");
 
+    
     function fetchData(endpoint) {
         fetch(`http://localhost:8000${endpoint}`)
             .then(res => {
@@ -24,4 +25,15 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("button-3").addEventListener("click", () => fetchData("/college"));
     document.getElementById("button-4").addEventListener("click", () => fetchData("/department"));
     document.getElementById("button-5").addEventListener("click", () => fetchData("/involvements"));
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('http://127.0.0.1:8000/visitor_count')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('visitor-count').innerText = `Visitor Count: ${data}`;
+        })
+        .catch(error => {
+            console.error('Error fetching visitor count:', error);
+        });
 });
